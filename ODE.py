374d103b419baab08eaebcc22cf34b573ff26562
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2017-10-02 16:37:47 -0300

    Inicializaci\'on. M\'odulo para ODE, y para p\'endulo simple

---
+++ ODE.py
@@ -0,0 +1,118 @@
+import numpy as np
+
+
+def theta( myfunc, y0, h, nt, th = 0, g = 1., l = 1. ):
+
+    """
+    Resuelve la ode definida por myfunc (f') usando el metodo theta.
+
+    Argumentos
+    h: paso de tiempo
+    nt: numero de intervalos temporales
+    th: theta
+
+    Devuelve un array con la solucion con todos los pasos de tiempo
+    """
+
+    y = np.zeros( (int(nt)+1, len(y0)))
+
+    y[0] = y0
+
+
+    
+    # Explicit Euler
+
+    if th == 0:
+
+        for i in range(nt):
+
+            y[i+1] = y[i] + h * myfunc(y[i],g,l)
+
+
+
+    # Other implicit
+            
+    else:
+        
+        for i in range(nt):
+
+            # dif = 1.
+
+            # y_aux = y[i]
+
+
+            # while (dif > 1.e-15):
+
+            #     y[i+1] = y[i]   +   h * (1-th) * myfunc(y[i],g,l)   +   h * th * myfunc(y_aux,g,l)
+
+            #     dif = np.linalg.norm( (y[i+1]-y_aux) )
+
+            #     y_aux = y[i+1]
+                
+            
+            for k in range(30):
+                            
+                y[i+1] = y[i]   +   h * (1-th) * myfunc(y[i],g,l)   +   h * th * myfunc(y[i+1],g,l)
+            
+        
+
+
+
+
+
+    return y
+
+
+    
+
+
+def RK4( myfunc, y0, h, nt, g = 1., l = 1. ):
+
+
+    """
+    Resuelve la ode definida por myfunc (f') usando el metodo theta.
+
+    Argumentos
+    h: paso de tiempo
+    nt: numero de intervalos temporales
+    th: theta
+
+    Devuelve un array con la solucion con todos los pasos de tiempo
+    """
+
+    y = np.zeros( (int(nt)+1, len(y0)))
+
+    y[0] = y0
+
+
+    # Arrays auxiliares
+    
+    k1 = np.zeros( len(y0) )
+    
+    k2 = np.zeros( len(y0) )
+
+    k3 = np.zeros( len(y0) )
+
+    k4 = np.zeros( len(y0) )
+
+
+    
+    
+    for i in range(nt):
+
+        k1 = h * myfunc( y[i], g, l )
+
+        k2 = h * myfunc( y[i] + 0.5 * k1, g, l )
+
+        k3 = h * myfunc( y[i] + 0.5 * k2, g, l )
+
+        k4 = h * myfunc( y[i] + k3, g, l )
+
+        y[i+1] = y[i]   +   ( k1 + 2.*k2 + 2.*k3 + k4 ) / 6.
+
+        
+
+    return y
+
+
+
---
+++ psimple.py
@@ -0,0 +1,239 @@
+import ODE
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+# Funcion derivada para el pendulo simple
+
+def pendulo(y,g,l):
+
+    yp = np.zeros(2)
+
+    yp[0] = y[1]
+
+    yp[1] = - g * np.sin(y[0]) / l
+
+    # yp[1] = - g * y[0] / l
+
+    return yp
+
+
+
+# Condicion inicial
+    
+y0 = np.zeros(2)
+
+y0[0] = np.radians(0.1)
+
+
+
+
+# Condiciones de simulacion
+
+tf = np.pi * 2
+
+orden = True
+
+
+
+if orden == False:
+    
+
+    # Condiciones de simulacion
+
+    nt = 30
+
+    tarray = np.linspace(0, tf, nt+1)
+
+    fig, axs = plt.subplots(1, 2)
+
+
+
+    # Euler Explicito
+
+    y = ODE.theta( pendulo, y0, tf/nt, nt, th = 0. )
+
+    axs[0].plot( tarray, y[:,0], label = 'Euler Explícito' )
+
+    axs[1].plot( tarray, y[:,1], label = 'Euler Explícito' )
+
+
+
+    # Euler Implicito
+
+    y = ODE.theta( pendulo, y0, tf/nt, nt, th = 1. )
+
+    axs[0].plot( tarray, y[:,0], label = 'Euler Implícito' )
+
+    axs[1].plot( tarray, y[:,1], label = 'Euler Implícito' )
+
+
+    
+    # Crank-Nicholson
+
+    y = ODE.theta( pendulo, y0, tf/nt, nt, th = 0.5 )
+
+    axs[0].plot( tarray, y[:,0], label = 'Crank-Nicholson' )
+
+    axs[1].plot( tarray, y[:,1], label = 'Crank-Nicholson' )
+
+
+    # Runge-Kutta 4
+
+    y = ODE.RK4( pendulo, y0, tf/nt, nt )
+
+    axs[0].plot( tarray, y[:,0], label = 'Runge-Kutta 4' )
+
+    axs[1].plot( tarray, y[:,1], label = 'Runge-Kutta 4' )
+        
+
+
+    # Grafico de theta
+    
+    axs[0].set_xlabel('t')
+
+    axs[0].set_ylabel(r'$\theta$')
+
+    axs[0].legend( loc = 'best' )
+
+    axs[0].axhline(y0[0], linestyle = '--')
+
+    axs[0].axhline(-y0[0], linestyle = '--')
+
+    
+
+    # Grafico de theta'
+    
+    axs[1].set_xlabel('t')
+
+    axs[1].set_ylabel(r'$\frac{d\theta}{dt}$')
+
+    axs[1].legend( loc = 'best' )
+
+
+    plt.show()
+
+
+    
+
+    
+else:
+
+
+    tlist = ( np.logspace(1,4,20) ).astype(int)
+
+
+
+    # Array de error
+
+    phaseError = np.zeros( (len(tlist), 4) )
+
+    ampError =  np.zeros( (len(tlist), 4) )
+
+
+
+    
+    for i, nt in enumerate(tlist):
+
+        
+        # Euler explicito
+        
+        y = ODE.theta( pendulo, y0, tf/int(nt), int(nt), th = 0. )
+
+        ampError[i][0] = abs( np.linalg.norm(y[0]) - np.linalg.norm(y[-1]) )
+
+        phaseError[i][0] = abs( np.arctan(y[-1][1]/y[-1][0]) )
+
+
+        # Euler implicito
+        
+        y = ODE.theta( pendulo, y0, tf/int(nt), int(nt), th = 1. )
+
+        ampError[i][1] = abs( np.linalg.norm(y[0]) - np.linalg.norm(y[-1]) )
+
+        phaseError[i][1] = abs( np.arctan(y[-1][1]/y[-1][0]) )
+
+        
+
+        # Crank-Nicholson
+        
+        y = ODE.theta( pendulo, y0, tf/int(nt), int(nt), th = 0.5 )
+
+        ampError[i][2] = abs( np.linalg.norm(y[0]) - np.linalg.norm(y[-1]) )
+
+        phaseError[i][2] = abs( np.arctan(y[-1][1]/y[-1][0]) )
+
+
+        # Runge-Kutta 4
+        
+        y = ODE.RK4( pendulo, y0, tf/int(nt), int(nt) )
+
+        ampError[i][3] = abs( np.linalg.norm(y[0]) - np.linalg.norm(y[-1]) )
+
+        phaseError[i][3] = abs( np.arctan(y[-1][1]/y[-1][0]) )
+        
+
+
+        
+
+
+    fig, axs = plt.subplots(1, 2)
+
+    axs[0].loglog( tf / tlist, ampError[:,0], label = 'Euler Explícito')
+    
+    axs[0].loglog( tf / tlist, ampError[:,1], label = 'Euler Implícito')
+
+    axs[0].loglog( tf / tlist, ampError[:,2], label = 'Crank-Nicholson')
+
+    axs[0].loglog( tf / tlist, ampError[:,3], label = 'Runge-Kutta')
+
+    axs[0].loglog( tf / tlist, tf / tlist, label = r'$O(h)$', linestyle = '--', color = 'k')
+
+    axs[0].loglog( tf / tlist, (tf / tlist)**2, label = r'$O(h^2)$', linestyle = ':', color = 'k')
+
+    axs[0].loglog( tf / tlist, (tf / tlist)**4, label = r'$O(h^4)$', linestyle = '-.', color = 'k')
+
+
+    axs[0].set_xlabel('h')
+
+    axs[0].set_ylabel(r'Error de amplitud')
+
+    axs[0].legend( loc = 'best' )
+
+    axs[0].grid()
+
+    
+
+
+    
+
+    axs[1].loglog( tf / tlist, phaseError[:,0], label = 'Euler Explícito')
+    
+    axs[1].loglog( tf / tlist, phaseError[:,1], label = 'Euler Implícito')
+
+    axs[1].loglog( tf / tlist, phaseError[:,2], label = 'Crank-Nicholson')
+
+    axs[1].loglog( tf / tlist, phaseError[:,3], label = 'Runge-Kutta')
+
+    axs[1].loglog( tf / tlist, tf / tlist, label = r'$O(h)$', linestyle = '--', color = 'k')
+
+    axs[1].loglog( tf / tlist, (tf / tlist)**2, label = r'$O(h^2)$', linestyle = ':', color = 'k')
+
+    axs[1].loglog( tf / tlist, (tf / tlist)**4, label = r'$O(h^4)$', linestyle = '-.', color = 'k')
+    
+
+    axs[1].set_xlabel('h')
+
+    axs[1].set_ylabel(r'Error de fase')
+
+    axs[1].legend( loc = 'best' )
+
+    axs[1].grid()
+
+    
+    plt.show()        
+
+
+        
